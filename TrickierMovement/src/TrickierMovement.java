697c69e742fd4a3f11306b4ebb4863470bc10c94
Sergey <serjio.nef@gmail.com>
2025-03-03 11:10:09 +0300

    feat: create new task #3.3 step 5 (TrickierMovement)
    * feat: create new package TrickierMovement and folder src
    * feat(package.TrickierMovement/src): add new file TrickierMovement.java
    * feat(package.TrickierMovement): add new folder Image
    * feat(package.TrickierMovement/Image): add new file Smile.png
    * feat(package.TrickierMovement): add new folder Gif
    * feat(package.TrickierMovement/Gif): add new file TrickierMovement.gif

---
+++ TrickierMovement/src/TrickierMovement.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class TrickierMovement extends JFrame{
+    private static final int width = 1015;
+    private static final int height = 990;
+    private static int x = 0;
+    private static int y = 0;
+    private static JLabel img;
+
+    public TrickierMovement() {
+        super("TrickierMovement");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(width, height);
+        setResizable(false);
+        setLayout(null);
+
+        img = new JLabel(new ImageIcon("TrickierMovement/Image/Smile.png"));
+        img.setBounds(x, y, 50, 50);
+        add(img);
+
+        ImageMove smileMove = new ImageMove();
+        addKeyListener(smileMove);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(TrickierMovement::new);
+    }
+
+
+    static class ImageMove extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case 37:
+                    x -= x - 50 < 0 ? -(width - 50) : 50;
+                    break;
+                case 38:
+                    y -= y - 50 < 0 ? -(height - 50) : 50;
+                    break;
+                case 39:
+                    x += x + 50 > width - 50 ? -(width - 50) : 50;
+                    break;
+                case 40:
+                    y += y + 50 > height - 50 ? -(height - 50) : 50;
+                    break;
+            }
+            img.setBounds(x, y, img.getWidth(), img.getHeight());
+        }
+    }
+}
